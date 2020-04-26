d93b9d1ee39b6989a9c16acd2f4245d7d86009f0
skrrrm <a@macbook-windows>
2020-04-26 14:51:30 +0200

    variable fix

--- KEAOpgave5.2/Program.cs
+++ KEAOpgave5.2/Program.cs
@@ -7,18 +7,18 @@ namespace KEAOpgave5._2
         static void Main(string[] args)
         {
             // Variables
-            int limit = 101;
+            int max = 101;
 
-            // a way to keep showing intro / info since we initially set limit to 101
-            while (limit > 100)
+            // a way to keep showing intro/info since we initially set limit to 101
+            while (max > 100)
             {
                 Console.WriteLine("Hi! Were generating Pythagorean triplets.");
-                Console.WriteLine("Please input a maximum for the triplets of max 100:");
+                Console.WriteLine("Please input a limit for the triplets of max 100:");
                 Console.WriteLine();
 
                 try // convert input to interger
                 {
-                    limit = Convert.ToInt32(Console.ReadLine()); 
+                    max = Convert.ToInt32(Console.ReadLine()); 
                 }
                 catch // if wrong input we try again
                 {
@@ -32,9 +32,9 @@ namespace KEAOpgave5._2
             // Max input for triplets is 100 just because of this.
             Console.WriteLine("    | A^2 |    | B^2 |    | SquareRoot of C |\n    |_____|____|_____|____|_________________|");
 
-            for (int a = 1; a < limit; a++) // running through a 
-                for (int b = 1; b < limit; b++) // running through b
-                    for (int c = 1; c < limit; c++) // running through c
+            for (int a = 1; a < max; a++) // running through a 
+                for (int b = 1; b < max; b++) // running through b
+                    for (int c = 1; c < max; c++) // running through c
                         if (a * a + b * b == c * c) // printing output if the numbers run through fits with pythagorian theorem
                             Console.WriteLine($"    | {a,2}  |    | {b,2}  |    | {c,9}       |"); // output
             Console.ReadLine(); // waiting for 'enter' to be able to view the output
